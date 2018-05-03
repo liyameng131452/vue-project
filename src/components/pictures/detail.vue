9a572469f92f09eebfce9b2b7f3fae2c68b28396
liyameng131452 <liyamengyw@163.com>
2018-05-03 13:30:30 +0800

    项目结构页面搭建完成

---
+++ src/components/pictures/detail.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>这是图片详情页</div>
+</template>
+<script>
+export default {
+  
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,68 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+//引入商品相关的组件
+import GoodList from "@/components/goods/list"
+import GoodDetail from "@/components/goods/detail"
+import NewsList from "@/components/news/list"
+import NewsDetail from "@/components/news/detail"
+import PictureList from "@/components/pictures/list"
+import PictureDetail from "@/components/pictures/detail"
+import Cart from "@/components/tabbar/cart"
+import Home from "@/components/tabbar/home"
+import Member from "@/components/tabbar/member"
+import Search from "@/components/tabbar/search"
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      redirect:'/home'
+    },
+    
+    //tabbar的
+    {
+      path: '/home',
+      component: Home
+    },
+    {
+      path: '/cart',
+      component: Cart
+    },
+    {
+      path: '/member',
+      component: Member
+    },
+    {
+      path: '/search',
+      component: Search
+    },
+    //商品的
+    {
+      path: '/goods',
+      component: GoodList
+    },
+    {
+      path: '/goods/detail',
+      component: GoodDetail
+    },
+    //新闻的
+    {
+      path: '/news',
+      component: NewsList
+    },
+    {
+      path: '/news/detail',
+      component: NewsDetail
+    },
+    //图片的
+    {
+      path: '/pics',
+      component: PictureList
+    },
+    {
+      path: '/pics/detail',
+      component: PictureDetail
+    },
+  ]
+})
